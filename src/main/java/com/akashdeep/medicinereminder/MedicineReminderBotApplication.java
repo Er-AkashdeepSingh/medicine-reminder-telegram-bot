2bfea79223e87cf76caa748b347a111f94999e35
ErAkashdeepSingh <akashdeepsingh6500@gmail.com>
2026-06-11 00:39:48 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/akashdeep/medicinereminder/MedicineReminderBotApplication.java
@@ -0,0 +1,13 @@
+package com.akashdeep.medicinereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineReminderBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineReminderBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akashdeep/medicinereminder/MedicineReminderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashdeep.medicinereminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineReminderBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
